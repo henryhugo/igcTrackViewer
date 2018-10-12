539ec0e0c4ee4b7374a7fb466eab6ff2fb6bc2b1
Hugo Henry <hugo.henry95@free.fr>
2018-10-12 16:20:37 +0200

    api/igc

--- main.go
+++ main.go
@@ -12,6 +12,9 @@ type API struct {
 	Version string //"version": "v1"
 
 }
+type igcFile struct {
+	Url string //a valid igc URL
+}
 
 func getApi(w http.ResponseWriter, r *http.Request) {
 	http.Header.Add(w.Header(), "content-type", "application/json")
@@ -25,8 +28,45 @@ func getApi(w http.ResponseWriter, r *http.Request) {
 
 }
 
+func igcHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "POST":
+		{
+			if r.Body == nil {
+				http.Error(w, "no JSON body", http.StatusBadRequest)
+				return
+			}
+			var igc igcFile
+			err := json.NewDecoder(r.Body).Decode(&igc)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+			}
+
+			/*
+				s := "http://skypolaris.org/wp-content/uploads/IGS%20Files/Madrid%20to%20Jerez.igc"
+				track, err := igc.ParseLocation(s)
+				if err != nil {
+					fmt.Errorf("Problem reading the track", err)
+				}
+
+				fmt.Printf("Pilot: %s, gliderType: %s, date: %s",
+					track.Pilot, track.GliderType, track.Date.String())*/
+		}
+	case "GET":
+		{
+			http.Header.Add(w.Header(), "content-type", "application/json")
+			//GET case
+		}
+	default:
+		http.Error(w, "not implemented yet", http.StatusNotImplemented)
+
+	}
+}
+
 func main() {
+
 	port := os.Getenv("PORT")
 	http.HandleFunc("/igcinfo/api", getApi)
+	http.HandleFunc("/igcinfo/api/igc", igcHandler)
 	http.ListenAndServe(":"+port, nil)
 }
