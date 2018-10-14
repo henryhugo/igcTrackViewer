89b49f76d221e40a18d23d5e18699d0866b3fb83
Hugo Henry <hugo.henry95@free.fr>
2018-10-14 12:51:43 +0200

    add test

--- main.go
+++ main.go
@@ -30,8 +30,8 @@ type igcDB struct {
 	igcs map[string]*igcFile
 }
 
-func (db *igcDB) add(igc igcFile, id string) {
-	db.igcs[id] = &igc
+func (db *igcDB) add(igc *igcFile, id string) {
+	db.igcs[id] = igc
 }
 
 func (db igcDB) Count() int {
@@ -81,8 +81,7 @@ func igcHandler(w http.ResponseWriter, r *http.Request) {
 			newId := Idstr + strValue
 			ids = append(ids, newId)
 			idCount += 1
-			//db.add(igc, newId)
-			db.igcs[newId] = &igcFile{igc.Url}
+			db.add(&igc, newId)
 			json.NewEncoder(w).Encode(newId)
 		}
 	case "GET":
