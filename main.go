c829d52f666939c9d3d292d0d17684c77fc140a1
Hugo Henry <hugo.henry95@free.fr>
2018-10-14 19:33:04 +0200

    field

--- main.go
+++ main.go
@@ -99,7 +99,7 @@ func igcHandler(w http.ResponseWriter, r *http.Request) {
 			parts := strings.Split(r.URL.Path, "/")
 			//fmt.Fprintf(w, "longueur : %d\n", len(parts))
 			//fmt.Fprintln(w, parts)
-			if len(parts) != 5 {
+			if len(parts) < 5 || len(parts) > 6 {
 				//deal with errors
 				fmt.Fprintln(w, "wrong numbers of parameters")
 				return
@@ -137,6 +137,13 @@ func igcHandler(w http.ResponseWriter, r *http.Request) {
 					fmt.Fprintln(w, "Use format id0 or id21 for exemple")
 				}
 			}
+			if parts[5] == "" {
+				fmt.Fprintln(w, "case field")
+				/*infoWanted := parts[5]
+				id := parts[4]*/
+
+			}
+
 		}
 	default:
 
